efcaf04691dabc725a486c63feb0a329d8740e44
Imraan Jacobs <imojacobs98@gmail.com>
2024-12-08 13:54:18 +0200

    Initial commit of Wordpress site

---
+++ wp-content/plugins/sureforms/assets/build/formSubmit.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => 'f5e43d4bdf3c3b0fc5bd');
---
+++ wp-content/plugins/sureforms/assets/build/settings.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => '977653c884e1aa5b8d79');
